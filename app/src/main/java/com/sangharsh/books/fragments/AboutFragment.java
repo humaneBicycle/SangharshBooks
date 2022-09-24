045d299337cd4b4fc95e03ac70c1152d519fd07e
aditya <adityarajputrana2016@gmail.com>
2022-09-24 16:27:03 +0530

    FINAL CTET : Aditya

--- app/src/main/java/com/sangharsh/books/fragments/AboutFragment.java
+++ app/src/main/java/com/sangharsh/books/fragments/AboutFragment.java
@@ -1,6 +1,7 @@
 package com.sangharsh.books.fragments;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -9,10 +10,17 @@ import android.os.Bundle;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.review.model.ReviewErrorCode;
+import com.google.gson.Gson;
 import com.sangharsh.books.R;
 public class AboutFragment extends Fragment {
 
@@ -45,8 +53,36 @@ public class AboutFragment extends Fragment {
 
         return v;
     }
-    public void rateApp()
+
+
+    public void rateApp(){
+        if (getActivity().getSharedPreferences("MY_PREF", Context.MODE_PRIVATE).getBoolean("is_rev", false)){
+            rateApp(0);
+            return;
+        }
+        getActivity().getSharedPreferences("MY_PREF", Context.MODE_PRIVATE).edit()
+                .putBoolean("is_rev", true).commit();
+        ReviewManager manager = ReviewManagerFactory.create(getActivity());
+        Task<ReviewInfo> request = manager.requestReviewFlow();
+        request.addOnCompleteListener(task -> {
+            if (task.isSuccessful() && task.isComplete()) {
+                ReviewInfo reviewInfo = task.getResult();
+                Task<Void> flow = manager.launchReviewFlow(getActivity(), reviewInfo);
+                flow.addOnCompleteListener((Task<Void> newTask) -> {
+                   if (!newTask.isSuccessful()){
+                       rateApp(0);
+                   }
+                });
+            } else {
+                rateApp(0);
+            }
+        });
+    }
+
+    public void rateApp(int error)
     {
+
+
         try
         {
             Intent rateIntent = rateIntentForUrl("market://details");
